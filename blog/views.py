c8292c52ecd70b5b2dd53e2ac62345ba1da175d5
bnv20 <amicobnv20@gmail.com>
2022-08-04 15:57:17 +0900

    포스트 입출력

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    
+class PostDetail(DetailView):
+    model = Post
+
